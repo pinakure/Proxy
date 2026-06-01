b55412331a85e3c0413a9d8b85e1f3ed3cb333f2
Pinakure (Smiker) <pinakure@gmail.com>
2026-06-01 03:03:44 +0200

    Initial Commit

---
+++ generate-certificate.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "docker compose run --rm certbot certonly --webroot --webroot-path=/var/www/certbot -d $1.ddns.net --email pinakure@gmail.com --agree-tos --no-eff-email"
+docker compose run --rm certbot certonly --webroot --webroot-path=/var/www/certbot -d $1.ddns.net --email pinakure@gmail.com --agree-tos --no-eff-email
---
+++ init-multiple-databases.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash
+set -e
+set -u
+
+#psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "CREATE DATABASE melodify";
+#psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "CREATE DATABASE m4gtracker";
+#psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "CREATE DATABASE warhammer";
+#psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "CREATE DATABASE trickynoid";
+#psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "CREATE DATABASE ticketshare";
+#psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "CREATE DATABASE keycloak";
+#psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "CREATE DATABASE trickynoid";
