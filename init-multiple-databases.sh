436e9099135a2161a83034cfdd45e0c291e9f21b
Pinakure (Smiker) <pinakure@gmail.com>
2026-06-02 01:46:31 +0200

    Separated conf files @ proxy.conf

--- init-multiple-databases.sh
+++ init-multiple-databases.sh
@@ -9,4 +9,4 @@ set -u
 #psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "CREATE DATABASE trickynoid";
 #psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "CREATE DATABASE ticketshare";
 #psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "CREATE DATABASE keycloak";
-#psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "CREATE DATABASE trickynoid";
+#psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "CREATE DATABASE erebus";
